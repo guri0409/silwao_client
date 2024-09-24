6998c51618474b595e5f51ee272969c0ca5013a0
guri0409 <gurigurdeepak@gmail.com>
2024-09-24 19:19:37 +0530

    add new features

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,6 +3,9 @@ import Navbar from './Navbar'
 import { Link } from 'react-router-dom'
 import white1 from '/public/images/white1.jpg'
 import white3 from '/public/images/white3.jpg'
+import one from '/images/one.png'
+import three from '/images/three.png'
+import two from '/images/two.png'
 import Customize from '/public/images/Customize.png'
 import resize from '/public/images/Resize.png'
 
@@ -10,24 +13,53 @@ export default function Home() {
   return (
     <>
     <Navbar/>
-    <div className='flex justify-center mt-8 items-center gap-6'>
+    <div className='flex justify-center mt-8 items-center gap-6 overflow-auto p-10 max-w-screen-sm'>
+        <Link to="/product-detail">
         <div className='flex flex-col items-center border gap-2'>
-            <img src={white1} className='h-28 w-32 md:h-72 md:w-80' alt="" srcset="" />
+            <img src={one} className='h-36 w-[100%] md:h-72 md:w-80 text-center' alt="" srcset="" />
             <h2 className='text-1xl font-semibold'>PatialaShahi Suit</h2>
             <p>Price: <span>1099/- Rs.</span></p>
         </div>
-        <div className='flex flex-col items-center border gap-2'>
-            <img src={white3} className='h-28 w-32 md:h-72 md:w-80' alt="" srcset="" />
+        </Link>
+       <Link>
+       <div className='flex flex-col items-center border gap-2'>
+            <img src={two} className='h-36 w-36
+             md:h-72 md:w-80' alt="" srcset="" />
             <h2 className='text-1xl font-semibold'>PatialaShahi Suit</h2>
             <h6>Price: <span>1199/- Rs.</span></h6>
         </div>
+       </Link>
+      <Link>
+      <div className='flex flex-col items-center border gap-2'>
+            <img src={three} className='h-36 w-36
+             md:h-72 md:w-80' alt="" srcset="" />
+            <h2 className='text-1xl font-semibold'>PatialaShahi Suit</h2>
+            <h6>Price: <span>1199/- Rs.</span></h6>
+        </div>
+      </Link>
+       <Link>
+       <div className='flex flex-col items-center border gap-2'>
+            <img src={three} className='h-36 w-36
+             md:h-72 md:w-80' alt="" srcset="" />
+            <h2 className='text-1xl font-semibold'>PatialaShahi Suit</h2>
+            <h6>Price: <span>1199/- Rs.</span></h6>
+        </div>
+       </Link>
+        <Link>
+        <div className='flex flex-col items-center border gap-2'>
+            <img src={three} className='h-36 w-36
+             md:h-72 md:w-80' alt="" srcset="" />
+            <h2 className='text-1xl font-semibold'>PatialaShahi Suit</h2>
+            <h6>Price: <span>1199/- Rs.</span></h6>
+        </div>
+        </Link>
 
     </div>
     <div className='flex justify-center mt-8 items-center gap-6'>
 
         <div className='flex flex-col items-center border gap-2'>
             <img src={Customize} className='h-36 w-32 md:h-64 md:w-64' alt="" srcset="" />
-            <Link to="/product-detail" className='h-10 w-20 bg-red-500 text-sm rounded-md text-center md:w-40'>Fully Customize</Link>
+            <Link to="/fully-customize" className='h-10 w-20 bg-red-500 text-sm rounded-md text-center md:w-40'>Fully Customize</Link>
         </div>
         <div className='flex flex-col items-center border gap-2'>
             <img src={resize} className='h-36 w-32 md:h-64 md:w-64' alt="" srcset="" />
